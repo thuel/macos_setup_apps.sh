acdbaf8e642696cda11641e3054eeee4198cf74b
thuel <thuel@me.com>
2017-12-18 21:24:03 +0100

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# further essentials
+brew cask install xquartz
+
 # productivity apps
 brew cask install evernote
-brew cask install lighttable
-brew cask install atom
 brew cask install libreoffice
+brew cask install mackeep
 
 # illustration / design apps
 brew cask install librecad
 brew cask install inkscape
 brew cask install scribus
 brew cask install gimp
+
+# development
+brew cask install lighttable
+brew cask install atom
+brew cask install pyenv
+# brew cask install python3
