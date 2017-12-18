3e53412e1f11f62b5fd33d4066107bc948356ddc
thuel <thuel@me.com>
2017-12-18 17:05:32 +0100

    Create macos_setup_apps.sh

---
+++ macos_setup_apps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install essential packages; first homebrew
+ruby -e "$(curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Setup homebrew/cask
+brew tap caskroom/cask
+brew install caskroom/cask/brew-cask
+
+# Install the useful software; to this end the file caskconfig.sh is sourced here
+. caskconfig.sh
