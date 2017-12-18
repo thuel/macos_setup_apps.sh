55d2994b2f7e55b1b5f95b0ea8e3a0b777d3fd38
thuel <thuel@me.com>
2017-12-18 17:09:23 +0100

    Create caskconfig.sh

---
+++ caskconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# productivity apps
+brew cask install evernote
+brew cask install lighttable
+brew cask install atom
+brew cask install libreoffice
+
+# illustration / design apps
+brew cask install librecad
+brew cask install inkscape
+brew cask install scribus
+brew cask install gimp
