d4a9d8ca641e8d52f50500824bcab2ffbf198aa1
thuel <thuel@me.com>
2017-12-19 20:37:06 +0100

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -35,7 +35,8 @@ pyenv install python3
 # ...
 
 # add apps to dock
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/MacPass</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
+defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/MacPass.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
+defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Nylas Mail.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
 defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/APPLICATION_NAME_HERE</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
 
 # reload dock
