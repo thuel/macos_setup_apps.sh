ccf8faa2dc00ecc2cf9b7fa20a3757c5ef5b0d2c
thuel <thuel@me.com>
2017-12-19 20:54:31 +0100

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -30,19 +30,3 @@ pyenv install python3
 
 # install mac appstore apps
 # ...
-
-# add clean dock.plist
-cp -f ./com.apple.dock.plist ~/Library/Preferences/
-
-# add apps to dock
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/MacPass.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Nylas Mail.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Lighttable.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Gimp.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Inkscape.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Librecad.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Libreoffice.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-defaults write com.apple.dock persistent-apps -array-add ‘<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/APPLICATION_NAME_HERE</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>’
-
-# reload dock
-killall Dock
